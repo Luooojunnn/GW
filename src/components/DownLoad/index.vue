585d4b92cfba73360c5ccbec11f2f6a9a312a18f
luojuna <luojuna@jiedaibao.com>
2018-10-22 19:32:39 +0800

    mockD

---
+++ src/components/DownLoad/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="pages-wrap">
+     <div class="all-pages">
+         <div v-for="item in pagesData" class="title-item">
+             <a @click="tableDownload(item.id)"> <img :src="item.icon" class="pre-cion"> {{item.title}} <span class="time">{{item.date}}</span></a>
+         </div>
+     </div>
+     <div class="pag">
+         <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="nums"
+            @current-change="changePage">
+        </el-pagination>    
+     </div>
+  </div>  
+</template>
+<script>
+import mix from '../../mixins/clickPage'
+export default {
+    data() {
+        return {
+            pagesData: [],
+            initNum: 0,
+            nums: 0
+        }
+    },
+  created() {
+    this.getData()
+  },
+  components:{
+  },
+  methods: {
+    getData() {
+      this.http
+      .get('http://localhost:9000/tableDownloadListApi', {
+          params: {
+              num: this.initNum
+          }
+      })
+      .then(res => {
+          if (+res.err.code === 200) {
+              this.nums = res.data.nums
+              this.pagesData = res.data.pages
+          } else {
+              this.$message.error(res.err.desc);
+          }
+      })
+      .catch(e => {
+          console.log(e)
+      })
+    },
+    changePage(v) {
+        this.initNum = +v - 1
+        this.getData()
+    },
+    tableDownload(id) {
+        console.log(id)
+    }
+  },
+  mixins: [mix]
+}
+</script>
+<style lang="scss" scoped>
+.pages-wrap {
+    .all-pages {
+        .title-item {
+            padding: 14px 0;
+            .time {
+                float: right;
+            }
+            a {
+                font-size: 16px;
+                font-weight: 400;
+                cursor: pointer;
+                padding-left: 1em;
+                margin: 0;
+                padding-right: 1em;
+            }
+            &:nth-child(2n+1) {
+                background-color: #b0bcdc;
+                border-radius: 4px;
+            }
+            &:hover {
+                text-decoration: underline;
+            }
+            .pre-cion {
+                display: inline-block;
+                width: 1em;
+            }
+        }
+    }
+    .pag {
+        text-align: right;
+        margin-top: 2em;
+    }
+}
+    
+</style>
