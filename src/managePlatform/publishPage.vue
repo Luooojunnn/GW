4986fccc84f45e3ff175a40b135e95a0a82cfe09
luojuna <luojuna@jiedaibao.com>
2018-10-24 20:14:32 +0800

    改字段

--- src/managePlatform/publishPage.vue
+++ src/managePlatform/publishPage.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="publish-wrap">
         <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="标题" prop='name'>
-                <el-input v-model="form.name"></el-input>
+            <el-form-item label="标题" prop='title'>
+                <el-input v-model="form.title"></el-input>
             </el-form-item>
             <el-form-item label="类别">
                 <el-select v-model="form.type" placeholder="请选择类别" prop='type'>
@@ -14,18 +14,18 @@
                     <el-option v-for="item in sourceArr" :key='item.val' :label="item.desc" :value="item.val"></el-option>
                 </el-select>
             </el-form-item>
-            <el-form-item label="发布人" prop='person'>
-                <el-input v-model="form.person"></el-input>
+            <el-form-item label="发布人" prop='editor'>
+                <el-input v-model="form.editor"></el-input>
             </el-form-item>
-            <el-form-item label="简介" prop='introduction'>
-                <el-input v-model="form.introduction"></el-input>
+            <el-form-item label="简介" prop='summary'>
+                <el-input v-model="form.summary"></el-input>
             </el-form-item>
             <el-form-item label="内容" prop='editorContent'>
                 <div id="editorElem"></div>
                 <!-- <ueditorOne ref="diseaseFileUeditor"></ueditorOne> -->
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="onSubmit" :disabled='!(this.form.name && this.form.type && this.form.person && this.form.introduction && this.form.source && this.form.editorContent)'>发布</el-button>
+                <el-button type="primary" @click="onSubmit" :disabled='!(this.form.title && this.form.type && this.form.editor && this.form.summary && this.form.source && this.form.editorContent)'>发布</el-button>
                 <el-button @click="resetForm('form')">重置</el-button>
             </el-form-item>
         </el-form>
@@ -39,10 +39,10 @@ export default {
     data() {
       return {
         form: {
-          name: '',
+          title: '',
           type: '',
-          person: '',
-          introduction: '',
+          editor: '',
+          summary: '',
           source: '',
           editorContent: ''
         },
